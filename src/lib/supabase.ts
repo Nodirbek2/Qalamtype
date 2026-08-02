d80ba374c1963c8fb9451d0868cf4c1d370c5dcf
Nodze <88966284+Nodirbek2@users.noreply.github.com>
2026-08-02 22:52:41 +0500

    last

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,10 +1,10 @@
 import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl =
-  import.meta.env.VITE_SUPABASE_URL || 'https://kaxqvpgbrqrxxknzhuip.supabase.co';
+  import.meta.env.VITE_SUPABASE_URL || 'https://btusuhujuegpglonfdvz.supabase.co';
 const supabaseAnonKey =
   import.meta.env.VITE_SUPABASE_ANON_KEY ||
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtheHF2cGdicnFyeHhrbnpodWlwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODU2ODAzOTAsImV4cCI6MjEwMTI1NjM5MH0.OQdmYEp2paJ2GVi-ZNve76Uv3z0kWd_Y54UdwYQz6kM';
+  'sb_publishable_spKF0PaacDnvwQwOQeGT5w_3uSO6ynd';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
 
